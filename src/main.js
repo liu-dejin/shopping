244e86982f942f863a8387255231ef3b6dd5ab1e
Your Name <liudejin@kis2.cn>
2025-07-02 14:02:17 +0800

    静态页面布局和图形验证码功能实现

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/utils/vant-ui'
+import '@/style/common.less'
 /* 全部导入vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,6 +1,7 @@
 // 按需导入
 import Vue from 'vue'
-import { Tabbar, TabbarItem } from 'vant'
+import { Tabbar, TabbarItem, NavBar } from 'vant'
 
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
+Vue.use(NavBar)
