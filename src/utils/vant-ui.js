5f6f41bf15427c47653005022fcf12347b8fb455
Your Name <liudejin@kis2.cn>
2025-07-03 17:21:11 +0800

    弹层和数字框

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,6 +1,6 @@
 // 按需导入
 import Vue from 'vue'
-import { Tabbar, TabbarItem, NavBar, Toast, Search, Swipe, SwipeItem, Grid, GridItem, Icon, Rate } from 'vant'
+import { Tabbar, TabbarItem, NavBar, Toast, Search, Swipe, SwipeItem, Grid, GridItem, Icon, Rate, ActionSheet } from 'vant'
 
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
@@ -13,3 +13,4 @@ Vue.use(Grid)
 Vue.use(GridItem)
 Vue.use(Icon)
 Vue.use(Rate)
+Vue.use(ActionSheet)
--- src/views/prodetail/index.vue
+++ src/views/prodetail/index.vue
@@ -74,17 +74,53 @@
         <van-icon name="shopping-cart-o" />
         <span>购物车</span>
       </div>
-      <div class="btn-add">加入购物车</div>
-      <div class="btn-buy">立刻购买</div>
+      <div @click="addFn" class="btn-add">加入购物车</div>
+      <div @click="buyFn" class="btn-buy">立刻购买</div>
     </div>
+<van-action-sheet v-model="showPannel" :title="mode === 'cart' ? '加入购物车' : '立刻购买'">
+  <div class="product">
+    <div class="product-title">
+      <div class="left">
+        <img :src="detail.goods_img" alt="">
+      </div>
+      <div class="right">
+        <div class="price">
+          <span>¥</span>
+          <span class="nowprice">{{detail.goods_price_min}}</span>
+        </div>
+        <div class="count">
+          <span>库存</span>
+          <span>{{detail.stock_total}}</span>
+        </div>
+      </div>
+    </div>
+    <div class="num-box">
+      <span>数量</span>
+      <!-- :value @input -->
+      <CountBox v-model="addCount"></CountBox>
+    </div>
+
+    <!-- 有库存才显示提交按钮 -->
+    <div class="showbtn" v-if="detail.stock_total>0">
+      <!-- mode加入购物车显示btn -->
+      <div class="btn" v-if="mode==='cart'">加入购物车</div>
+      <div class="btn now" v-else>立刻购买</div>
+    </div>
+    <div class="btn-none" v-else>该商品已抢完</div>
+  </div>
+</van-action-sheet>
   </div>
 </template>
 
 <script>
 import { getProDetail, getProComments } from '@/api/product'
 import defaultImg from '@/assets/default-avatar.png'
+import CountBox from '@/components/CountBox.vue'
 export default {
   name: 'ProDetail',
+  components: {
+    CountBox
+  },
   data () {
     return {
       images: [],
@@ -92,7 +128,10 @@ export default {
       detail: [],
       total: 0, // 评价总数
       commentList: [], // 评价列表
-      defaultImg
+      defaultImg, // 默任头像
+      showPannel: false, // 显示隐藏弹层
+      mode: 'cart', // 标记弹层
+      addCount: 1 //  数字框绑定的数据
     }
   },
   computed: {
@@ -113,11 +152,20 @@ export default {
       const { data: { detail } } = await getProDetail(this.goodsId)
       this.detail = detail
       this.images = detail.goods_images
+      console.log(detail)
     },
     async getComments () {
       const { data: { list, total } } = await getProComments(this.goodsId, 2)
       this.commentList = list
       this.total = total
+    },
+    addFn () {
+      this.mode = 'cart'
+      this.showPannel = true
+    },
+    buyFn () {
+      this.mode = 'buyNow'
+      this.showPannel = true
     }
   }
 }
@@ -268,5 +316,54 @@ export default {
 
 .tips {
   padding: 10px;
+}
+
+// 弹层样式
+.product {
+  .product-title {
+    display: flex;
+    .left {
+      img {
+        width: 90px;
+        height: 90px;
+      }
+      margin: 10px;
+    }
+    .right {
+      flex: 1;
+      padding: 10px;
+      .price {
+        font-size: 14px;
+        color: #fe560a;
+        .nowprice {
+          font-size: 24px;
+          margin: 0 5px;
+        }
+      }
+    }
+  }
+
+  .num-box {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    align-items: center;
+  }
+
+  .btn, .btn-none {
+    height: 40px;
+    line-height: 40px;
+    margin: 20px;
+    border-radius: 20px;
+    text-align: center;
+    color: rgb(255, 255, 255);
+    background-color: rgb(255, 148, 2);
+  }
+  .btn.now {
+    background-color: #fe5630;
+  }
+  .btn-none {
+    background-color: #cccccc;
+  }
 }
 </style>
